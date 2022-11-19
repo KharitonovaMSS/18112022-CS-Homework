19a286826d14c6eaaf112f33ddf530ea18b50b57
Maria <mineeva_maria@mail.ru>
2022-11-19 10:25:11 +0300

    The first task is solved

---
+++ Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int randomNum = new Random().Next(100,1000);
+Console.WriteLine(randomNum);
+int num = (randomNum / 10 - randomNum / 100 * 10);
+Console.WriteLine(num);
